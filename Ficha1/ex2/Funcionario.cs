d528b78d478a99fc7d0cb387ab19f21e374c9819
Rafael Pereira <rafaelpereiraesjac@gmail.com>
2025-09-25 11:59:08 +0100

    ex2

---
+++ Ficha1/ex2/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿namespace EmpresaFuncionarios;
+
+public abstract class Funcionario
+{
+    public string Nomes { get; }
+
+    public string Apelidos { get; }
+
+    public string NIF { get; }
+    //constructor
+    public Funcionario(string nomes, string apelidos, string nif)
+    {
+        Nomes = nomes;
+        Apelidos = apelidos;
+        NIF = nif;
+    }
+
+    public abstract double Vencimento();
+
+    public override string ToString()
+    {
+        return $"Nome= {Nomes} {Apelidos} \n NIF= {NIF}\n";
+    }
+}
---
+++ Ficha1/ex2/FuncionarioComissao.cs
@@ -0,0 +1,53 @@
+﻿namespace EmpresaFuncionarios;
+
+public class FuncionarioComissao : Funcionario
+{
+    private double vendas_totais;
+    private double comissao_vendas;
+    //construtor
+
+    public FuncionarioComissao(string nomes, string apelidos, string nif,
+        double vendas_totais_entra, double comissao_vendas_entra) : base(nomes, apelidos, nif)
+    {
+        vendas_totais = vendas_totais_entra;        // valida as vendas
+        comissao_vendas = comissao_vendas_entra;    // valida a comissao
+    }
+
+    public double VendasTotais
+    {
+        get { return vendas_totais; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    value, $"{nameof(vendas_totais)} tem que ser >= 0");
+            }
+            vendas_totais = value;
+        }
+    }
+
+    public double ComissaoVendas
+    {
+        get { return comissao_vendas; }
+        set
+        {
+            if (value <= 0 || value >= 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    value, $"{nameof(ComissaoVendas)} tem que ser > 0 e < 1");
+            }
+            comissao_vendas = value;
+        }
+    }
+
+    public override double Vencimento() { return comissao_vendas * vendas_totais; }
+
+    public override string ToString()
+    {
+        return $"Funcionario (por comissao): {base.ToString()}\n"
+            + $"Vendas Totais: {vendas_totais:F2}\n"
+            + $"Comissao de Vendas: {comissao_vendas:F2}\n"
+            + $"Componente do Vencimento Por Comissões: {comissao_vendas * vendas_totais}";
+    }
+}
