fcffda7e725f7a5e4c8122e33517fe125653cf02
Rafael Pereira <rafaelpereiraesjac@gmail.com>
2025-11-12 22:54:43 +0000

    Ficha 6 *on the works*

--- Ficha5_(4V2)/GestaoLoja/GestaoLoja/Entities/Categoria.cs
+++ Ficha5_(4V2)/GestaoLoja/GestaoLoja/Entities/Categoria.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
-
+using System.ComponentModel.DataAnnotations;
 namespace GestaoLoja.Entities;
 public class Categoria
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Mete nome nisso senão não deixo!")]
     public string? Nome { get; set; }
     public int? Ordem { get; set; }
     public string? UrlImagem { get; set; }
